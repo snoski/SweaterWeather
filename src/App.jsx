7a516d95e8e1e45502fca3b2bd2da976ebd1670b
snoski <jeff.b.carter@gmail.com>
2026-07-19 19:45:17 -0500

    working hooks - diff rows

--- src/App.jsx
+++ src/App.jsx
@@ -20,28 +20,15 @@ function App() {
   });
   const [rows, setRows] = useState(100);
   const [numCols, setNumCols] = useState(10);
+  
+  const rowProps = [{color:color,maxChg:maxChg,numCols:numCols,row:0}];
 
-  // const getDir = () => Math.sign(0.5 - Math.random());
-
-  // // useEffect(() => {
-  // //   console.log("old color: ",color);
-  // //   console.log("")
-  // // },[color]);
-
-
-  // const getNewColor = (color,maxChg) => {
-  //   const newColor = {};   
-  //   newColor.r = Math.min(Math.max(color.r + (getDir() * maxChg.r * Math.random()),0),255); 
-  //   newColor.g = Math.min(Math.max(color.g + (getDir() * maxChg.g * Math.random()),0),255); 
-  //   newColor.b = Math.min(Math.max(color.b + (getDir() * maxChg.b * Math.random()),0),255);
-
-  //   return newColor;
-  // }
-
-  const rowProps = [];
-
-  for (let i = 1; i < (rows + 1); i++) {
-    rowProps.push({color:color,maxChg:maxChg,numCols:numCols,row:i});
+  for (let i = 1; i < (rows); i++) {
+    rowProps.push({
+      color: useNewColor(rowProps[i-1].color,maxChg),
+      maxChg:maxChg,
+      numCols:numCols,
+      row:i});
   }
   
   return (
--- src/components/ColorRow.jsx
+++ src/components/ColorRow.jsx
@@ -2,23 +2,6 @@ import { useState, useEffect } from 'react';
 import useNewColor from '../hooks/useNewColor.js';
 
 function ColorRow(props) {
-  
-
-  // const getDir = () => Math.sign(0.5 - Math.random());
-
-  // useEffect(() => {
-  //   console.log("ColorRow props: ",props);
-  // },[props]);
-
-
-  // const getNewColor = (color,maxChg) => {
-  //   const newColor = {};   
-  //   newColor.r = Math.min(Math.max(color.r + (getDir() * maxChg.r * Math.random()),0),255); 
-  //   newColor.g = Math.min(Math.max(color.g + (getDir() * maxChg.g * Math.random()),0),255); 
-  //   newColor.b = Math.min(Math.max(color.b + (getDir() * maxChg.b * Math.random()),0),255);
-
-  //   return newColor;
-  // }
 
   const colorArr = [props.color];
   for (let i = 1; i < (props.numCols + 1); i++) {
